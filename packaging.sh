2fc27f567fe0556827dda15cf456b02f06341b76
YounSeok.Shim <aster9725@innogrid.com>
2020-08-05 13:00:27 +0900

    Add packaging.sh

---
+++ packaging.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PWD=$(pwd)
+FILE_LIST="$(ls $PWD/tmp_melody/*.proto)"
+
+if [ $# -ne 1 ]
+then
+	echo "Enter package name"
+	exit 1
+else
+	for file in $FILE_LIST
+	do
+		filename=$(basename -- $file)
+		echo "option go_package=\"hcc/$1/action/grpc/rpc${filename%.*}\";" >> $file
+	done
+fi
